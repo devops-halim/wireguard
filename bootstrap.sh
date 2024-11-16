574c50414f77a6cac672ce74c9c62c51dc64153e
devops-halim <a.alnasani@outlook.com>
2024-11-16 02:00:33 +0100

    ✅ Docker GPG key & Repo

--- bootstrap.sh
+++ bootstrap.sh
@@ -21,11 +21,17 @@ sudo apt update && sudo apt upgrade -y
 # sudo passwd "$new_user"
 # sudo usermod -aG sudo "$new_user"
 
-sudo su - $new_user
+#sudo su - $new_user
+
 # Docker und Docker Compose sicherstellen
 echo "Stelle sicher, dass Docker und Docker Compose installiert sind..."
 if ! command -v docker &> /dev/null; then
     echo "Docker ist nicht installiert. Installiere Docker..."
+    # Docker GPG-Schlüssel hinzufügen
+    sudo mkdir -m 0755 -p /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/$(lsb_release -si | tr '[:upper:]' '[:lower:]')/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    #  Docker Repository hinzufügen
+    echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/$(lsb_release -si | tr '[:upper:]' '[:lower:]') $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     sudo apt update
     sudo apt install -y docker-ce docker-ce-cli containerd.io
 fi
